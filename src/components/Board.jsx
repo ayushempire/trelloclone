2fc2a9ec6350a1889dbcb8f5600a5c6a595406bb
Ayush <ayushempire03@gmail.com>
2023-09-07 13:23:10 +0530

    made  drag and drop within column

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,10 +4,24 @@ import { DragDropContext } from "react-beautiful-dnd";
 
 export default function Board() {
   // column array basic
-  const [todos, settodos] = useState(["item1", "item2", "item3"]);
+  const [todos, settodos] = useState(["item 1", "item 2", "item 3"]);
 
-  // ondrag end function
-  const onDragEnd = () => null;
+  //   /ondrag end function
+  const onDragEnd = ({ source, destination }) => {
+    // valid destination
+    if (destination === undefined || destination === null) return;
+
+    // make sure we moving item
+    if (destination.index === source.index) return;
+
+    // move item in list or column
+    let newtodos = [...todos]; // made instance of todo array
+    newtodos.splice(source.index, 1); // made a new array without source
+
+    newtodos.splice(destination.index, 0, todos[source.index]); // spilce that with destination and source
+
+    settodos(newtodos); // setting that newarray to main todos
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
