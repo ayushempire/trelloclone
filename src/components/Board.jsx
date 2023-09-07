89b4d15366d002861e30094fa10ee7d547389fb2
Ayush <ayushempire03@gmail.com>
2023-09-07 16:09:38 +0530

    made item drag -drop in same column

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,24 +3,72 @@ import Column from "./Column";
 import { DragDropContext } from "react-beautiful-dnd";
 
 export default function Board() {
+  // for multiple columns made an usestate
+  const initialColumns = {
+    todo: {
+      id: "todo",
+      list: ["item 1", "item 2", "item 3"],
+    },
+    doing: {
+      id: "doing",
+      list: [],
+    },
+    done: {
+      id: "done",
+      list: [],
+    },
+  };
+
   // column array basic
-  const [todos, settodos] = useState(["item 1", "item 2", "item 3"]);
+  const [todos, settodos] = useState(initialColumns);
 
   //   /ondrag end function
   const onDragEnd = ({ source, destination }) => {
     // valid destination
-    if (destination === undefined || destination === null) return;
+    if (destination === undefined || destination === null) {
+      console.log("wrong in destination");
+      return null;
+    }
 
-    // make sure we moving item
-    if (destination.index === source.index) return;
+    //    { // move item in list or column
+    //     // let newtodos = [...todos]; // made instance of todo array
+    //     // newtodos.splice(source.index, 1); // made a new array without source
+    //     // newtodos.splice(destination.index, 0, todos[source.index]); // spilce that with destination and source
+    //     // settodos(...newtodos); // setting that newarray to main todos
+    //    }
+    //  ? If the source and destination columns are the same
+    // ? AND if the index is the same, the item isn't moving
+    if (
+      source.droppableId === destination.droppableId &&
+      destination.index === source.index
+    ) {
+      return null;
+    }
 
-    // move item in list or column
-    let newtodos = [...todos]; // made instance of todo array
-    newtodos.splice(source.index, 1); // made a new array without source
+    // todo:  set start and end variables
+    const start = todos[source.droppableId];
+    const end = todos[destination.droppableId];
 
-    newtodos.splice(destination.index, 0, todos[source.index]); // spilce that with destination and source
+    // ? if start is the same as end then we are in the same column
+    if (start === end) {
+      // todo: move item within list
+      let newtodos = [...start.list]; // made instance of todo array
 
-    settodos(newtodos); // setting that newarray to main todos
+      newtodos.splice(source.index, 1); // made a new array without source
+
+      newtodos.splice(destination.index, 0, start.list[source.index]); // spilce that with destination and source
+
+      //   console.log(newtodos);
+      //   create new copy of object
+      const newCol = {
+        id: start.id,
+        list: newtodos,
+      };
+      console.log(newCol);
+
+      //   // update the state
+      settodos((state) => ({ ...state, [newCol.id]: newCol }));
+    }
   };
 
   return (
@@ -35,7 +83,9 @@ export default function Board() {
           gap: "8px",
         }}
       >
-        <Column list={todos} />
+        {Object.values(todos).map((col) => (
+          <Column col={col} key={col.id} />
+        ))}
       </div>
     </DragDropContext>
   );
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -2,19 +2,24 @@ import React from "react";
 import Item from "./Item";
 import { Droppable } from "react-beautiful-dnd";
 
-export default function Column({ list }) {
+export default function Column({ col: { list, id } }) {
   return (
-    <Droppable droppableId="col-1">
+    <Droppable droppableId={id}>
       {(provided) => (
-        <div
-          className="d-flex flex-column"
-          {...provided.droppableProps}
-          ref={provided.innerRef}
-        >
-          {list.map((text, index) => (
-            <Item key={text} text={text} index={index} />
-          ))}
-          {provided.placeholder}
+        <div className="d-flex flex-column mt-2 bg-light border-dark">
+          <h2 className="text-center text-white bg-dark pb-2">{id}</h2>
+
+          <div
+            className="d-flex flex-column p-2"
+            style={{ minHeight: "fit-content" }}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {list.map((text, index) => (
+              <Item key={text} text={text} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
         </div>
       )}
     </Droppable>
