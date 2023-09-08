20a5bad6e7cc8773e9f84b29545df901c40b727e
Ayush <ayushempire03@gmail.com>
2023-09-09 00:40:04 +0530

    added routes and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Board from "./components/Board";
+import HomePage from "./screens/Homepage";
 import Navbar from "./screens/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Board />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/Board" element={<Board />} />
+        </Routes>
+      </Router>
     </>
   );
 }
