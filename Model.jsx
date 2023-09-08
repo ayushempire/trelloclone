1ed0e6af24645f4d2e61d5b6c07456a6ca250248
Ayush <ayushempire03@gmail.com>
2023-09-08 21:11:48 +0530

    eidt item partially not saved

--- Model.jsx
+++ Model.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 
 const MODAL_STYLES = {
@@ -22,7 +22,14 @@ const OVERLAY_STYLES = {
   zIndex: 1000,
 };
 
-export default function Model({ onClose }) {
+export default function Model({ item: { id, title, desc }, onClose }) {
+  // usestate htmlFor edit task
+  const [edittask, setedittask] = useState({
+    id: id,
+    title: title,
+    desc: desc,
+  });
+
   return ReactDom.createPortal(
     <>
       <div style={OVERLAY_STYLES} />
@@ -35,7 +42,51 @@ export default function Model({ onClose }) {
           {" "}
           X{" "}
         </button>
-        <h1>hello</h1>
+        <div className="container">
+          <h1 className="text-center">Edit task</h1>
+
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Title
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                value={(setedittask.title, edittask.title)}
+                onChange={(e) =>
+                  setedittask({ ...edittask, title: e.target.value })
+                }
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                description
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputPassword1"
+                value={(setedittask.desc, edittask.desc)}
+                onChange={(e) =>
+                  setedittask({ ...edittask, desc: e.target.value })
+                }
+              />
+            </div>
+
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={() => {
+                console.log(edittask);
+              }}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </>,
     document.getElementById("edit-root")
